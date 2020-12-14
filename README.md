9d461427e3d85b95bb2d24d2535665f2d26eec79
Alex Olkhovskiy <lxol@users.noreply.github.com>
2020-12-14 15:24:33 +0000

    [DC-2891] configure microservice as per instruction
    https://confluence.tools.tax.service.gov.uk/display/DCT/Create+a+new+microservice

--- README.md
+++ README.md
@@ -3,6 +3,27 @@
 
 This is a placeholder README.md for a new repository
 
+
+## Swagger UI
+
+To get a feel for the endpoints exposted by `releases-api`, you can use
+[Swagger UI](https://swagger.io/tools/swagger-ui/).
+
+You an access it at https://catalogue-labs.tax.service.gov.uk/swagger-ui/ :)
+
+### Running swagger locally
+
+To load the UI, run `./swagger-ui.sh` after you have `releases-api` running.
+
+> You need to have docker setup for this. You can populate the version used in the UI if you run with
+`sbt 'run -DAPP_VERSION=<version>'. This gets injected automatically on k8s`
+
+
+Then just open the UI which will be running here http://localhost:8009! 
+
+> The `swagger.json` is served from `releases-api` at `/swagger/swagger.json`.
+ 
+
 ### License
 
 This code is open source software licensed under the [Apache 2.0 License]("http://www.apache.org/licenses/LICENSE-2.0.html").
--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,14 +7,29 @@ object AppDependencies {
 
   val compile = Seq(
     "uk.gov.hmrc"             %% "bootstrap-backend-play-27"  % "3.2.0",
+    "com.iterable"            %% "swagger-play"               % "2.0.1",
     "uk.gov.hmrc"             %% "simple-reactivemongo"       % "7.31.0-play-27"
   )
 
+  // The fork of swagger-play requires a version of jackson-databind version >= 2.9.0 and < 2.10.0
+  // Other libraries pulling in later jackson-databind include http-verbs and logback-json-logger
+  val dependencyOverrides: Seq[ModuleID] = Seq(
+    "com.fasterxml.jackson.core"     % "jackson-databind"        % "2.9.9"
+  )
+
   val test = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-test-play-27"   % "3.2.0" % Test,
-    "org.scalatest"           %% "scalatest"                % "3.2.3"  % Test,
-    "com.typesafe.play"       %% "play-test"                % current  % Test,
-    "com.vladsch.flexmark"    %  "flexmark-all"             % "0.36.8" % "test, it",
-    "org.scalatestplus.play"  %% "scalatestplus-play"       % "4.0.3"  % "test, it"
+    // "uk.gov.hmrc"             %% "bootstrap-test-play-27"   % "3.2.0" % Test,
+    // "org.scalatest"           %% "scalatest"                % "3.2.3"  % Test,
+    // "com.typesafe.play"       %% "play-test"                % current  % Test,
+    // "com.vladsch.flexmark"    %  "flexmark-all"             % "0.36.8" % "test, it",
+    // "uk.gov.hmrc"            %% "service-integration-test" % "0.13.0-play-27" % "test, it",
+    // "org.scalatestplus.play"  %% "scalatestplus-play"       % "4.0.3"  % "test, it",
+    // "org.pegdown"            % "pegdown"                   % "1.6.0"          % "test, it"
+
+    "uk.gov.hmrc"            %% "bootstrap-test-play-27"   % "3.2.0"          % Test,
+    "com.typesafe.play"      %% "play-test"                % current          % Test,
+    "org.scalatestplus.play" %% "scalatestplus-play"       % "4.0.3"          % "test, it",
+    "uk.gov.hmrc"            %% "service-integration-test" % "0.13.0-play-27" % "test, it",
+    "org.pegdown"            % "pegdown"                   % "1.6.0"          % "test, it"
   )
 }
