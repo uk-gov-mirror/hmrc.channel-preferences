0eaa276c1fabf665840999df89adbc93ff94cb23
Alex Olkhovskiy <lxol@users.noreply.github.com>
2020-12-31 07:03:36 +0000

    Revert "[DC-2998] Refactor to use WS client for proxy (#8)"
    This reverts commit d2f86f09db189f0663a704209f8cf36616a86211.

--- app/uk/gov/hmrc/channelpreferences/controllers/ProxyController.scala
+++ app/uk/gov/hmrc/channelpreferences/controllers/ProxyController.scala
@@ -16,13 +16,20 @@
 
 package uk.gov.hmrc.channelpreferences.controllers
 
+import java.util.UUID.randomUUID
+
+import akka.stream.scaladsl.Source
+import akka.util.ByteString
+import javax.inject.Inject
+import org.slf4j.MDC
 import play.api.Logger
+import play.api.libs.streams.Accumulator
 import play.api.mvc._
 import uk.gov.hmrc.channelpreferences.connectors.OutboundProxyConnector
 import uk.gov.hmrc.play.bootstrap.backend.controller.BackendController
 
-import javax.inject.Inject
 import scala.concurrent.ExecutionContext
+
 @SuppressWarnings(Array("org.wartremover.warts.ImplicitParameter"))
 class ProxyController @Inject()(
   outboundConnector: OutboundProxyConnector
@@ -31,12 +38,32 @@ class ProxyController @Inject()(
 
   val log: Logger = Logger(this.getClass)
 
-  def proxy(path: String): Action[RawBuffer] =
-    Action.async(controllerComponents.parsers.raw) { implicit request =>
+  @SuppressWarnings(Array("org.wartremover.warts.Any", "org.wartremover.warts.Nothing"))
+  val streamedBodyParser: BodyParser[Source[ByteString, Any]] =
+    BodyParser(_ => Accumulator.source[ByteString].map((x: Source[ByteString, Any]) => Right.apply(x)))
+
+  @SuppressWarnings(Array("org.wartremover.warts.Any"))
+  def proxy(path: String): Action[Source[ByteString, _]] =
+    Action.async(streamedBodyParser) { implicit request =>
+      populateMdc(request)
+
+      log.info(s"Inbound Request: ${request.method} ${request.uri}")
+
       outboundConnector.proxy(request).recover {
         case ex: Exception =>
           log.error(s"An error occurred proxying $path", ex)
           InternalServerError(ex.getMessage)
       }
     }
+
+  private def populateMdc(implicit request: Request[Source[ByteString, _]]): Unit = {
+    val extraDiagnosticContext = Map(
+      "transaction_id"                                         -> randomUUID.toString
+    ) ++ request.headers.get(USER_AGENT).toList.map(USER_AGENT -> _)
+
+    (hc.mdcData ++ extraDiagnosticContext).foreach {
+      case (k, v) => MDC.put(k, v)
+    }
+  }
+
 }
